e19a528321fe31b6c8846a70a2d635d6c5c51843
Juanan <juanantonioamillopez@gmail.com>
2026-02-02 14:58:34 +0100

    FEAT: Formulario con FORMIK

--- src/components/FormFormik.jsx
+++ src/components/FormFormik.jsx
@@ -1,9 +1,24 @@
+import { useFormik } from 'formik';
+
 const FormFormik = () => {
+  const formik = useFormik({
+    initialValues: {
+      nombre: '',
+      email: '',
+      accept: false,
+    },
+
+    onSubmit: (values) => {
+      console.log('Formulario FORMIK enviado:', values);
+    },
+  });
+
   return (
     <div>
       <h2>Formulario con Formik</h2>
 
-      <form action=''>
+      {/* La función handleSubmit ya viene incluida en el paquete formik */}
+      <form onSubmit={formik.handleSubmit}>
         <div
           style={{
             display: 'flex',
@@ -14,17 +29,23 @@ const FormFormik = () => {
           }}>
           <div style={{ display: 'flex' }}>
             <label htmlFor='nombre'>Nombre:</label>
-            <input type='text' id='nombre' name='nombre' />
+            <input type='text' id='nombre' name='nombre' value={formik.values.nombre} onChange={formik.handleChange} />
           </div>
 
           <div style={{ display: 'flex' }}>
             <label htmlFor='email'>Email:</label>
-            <input type='text' id='email' name='email' />
+            <input type='text' id='email' name='email' value={formik.values.email} onChange={formik.handleChange} />
           </div>
 
           <div style={{ display: 'flex' }}>
             <label htmlFor='accept'>Aceptar términos:</label>
-            <input type='checkbox' id='accept' name='accept' />
+            <input
+              type='checkbox'
+              id='accept'
+              name='accept'
+              checked={formik.values.accept}
+              onChange={formik.handleChange}
+            />
           </div>
           <input type='submit' value='Enviar' />
         </div>
