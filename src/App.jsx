c8a0d21e64f7ff7e3b580b9ad336c42f539a3c24
Juanan <juanantonioamillopez@gmail.com>
2026-02-03 11:52:43 +0100

    FEAT: Formularios en rutas distintas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import FormBasico from './components/FormBasico';
 import FormReactHookForm from './components/FormReactHookForm';
 import FormFormik from './components/FormFormik';
@@ -7,28 +7,51 @@ import './App.css';
 
 function App() {
   const [route, setRoute] = useState('home');
+  const [path, setPath] = useState(window.location.pathname);
 
-  const renderComponent = () => {
-    switch(route) {
-      case 'form-basico':
-        return <FormBasico />;
-      case 'form-hook-form':
-        return <FormReactHookForm />;
-      case 'formik':
-        return <FormFormik />;
-      default:
-        return null;
-    }
+  const goTo = (path) => {
+    window.history.pushState({}, '', path);
+    window.dispatchEvent(new PopStateEvent('popstate'));
   };
+
+  // const renderComponent = () => {
+  //   switch (path) {
+  //     case 'form-basico':
+  //       return <FormBasico />;
+  //     case 'form-hook-form':
+  //       return <FormReactHookForm />;
+  //     case 'formik':
+  //       return <FormFormik />;
+  //     default:
+  //       return null;
+  //   }
+  // };
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
 
   return (
     <>
       <h1>Estudio Básico de Formularios</h1>
-      <button onClick={() => setRoute('form-basico')}>Formulario Básico</button>
-      <button onClick={() => setRoute('form-hook-form')}>Formulario React Hook Form</button>
-      <button onClick={() => setRoute('formik')}>Formulario Formik</button>
-      
-      {renderComponent()}
+      <button onClick={() => goTo('/form-basico')}>Formulario Básico</button>
+      <button onClick={() => goTo('/form-hook-form')}>Formulario React Hook Form</button>
+      <button onClick={() => goTo('/formik')}>Formulario Formik</button>
+      <button onClick={() => goTo('/')}>Home</button>
+
+      {/* {renderComponent()} */}
+
+        {path === "/form-basico" && <FormBasico />}
+      {path === "/form-hook-form" && <FormReactHookForm />}
+      {path === "/formik" && <FormFormik />}
     </>
   );
 }
