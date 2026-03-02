0ebecf5e27724e6d2891d4cc6204eb6059314347
Juanan <juanantonioamillopez@gmail.com>
2026-03-02 12:44:26 +0100

    FEAT: Formulario con Formik + Material UI

--- src/components/FormFormikMUI.jsx
+++ src/components/FormFormikMUI.jsx
@@ -1,3 +1,5 @@
+import { Box, Button, Checkbox, FormControlLabel, FormHelperText, TextField, Typography } from '@mui/material';
+
 import { useFormik } from 'formik';
 import { getSaludo } from '../api/getSaludo';
 import * as Yup from 'yup';
@@ -36,32 +38,52 @@ export default function FormFormikMUI() {
 
   return (
     <div>
-      <h2>Formulario con Formik y Material UI</h2>
+      <Typography variant='h2' component='h2' sx={{ fontSize: '1.5em' }} gutterBottom>
+        Formulario con Formik y Material UI
+      </Typography>
 
       <form onSubmit={formik.handleSubmit}>
-        <div className='form-group'>
-          <label htmlFor='nombre'>Nombre:</label>
-          <input type='text' id='nombre' name='nombre' value={formik.values.nombre} onChange={formik.handleChange} />
-          {formik.touched.nombre && formik.errors.nombre && <span className='error'>{formik.errors.nombre}</span>}
-        </div>
-
-        <div style={{ display: 'flex' }} className='form-group'>
-          <label htmlFor='email'>Email:</label>
-          <input type='text' id='email' name='email' value={formik.values.email} onChange={formik.handleChange} />
-        </div>
-
-        <div className='form-group checkbox'>
-          <label htmlFor='accept'>Aceptar términos:</label>
-          <input
-            type='checkbox'
-            id='accept'
-            name='accept'
-            checked={formik.values.accept}
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          {/* Nombre */}
+          <TextField
+            label='Nombre'
+            id='nombre'
+            name='nombre'
+            value={formik.values.nombre}
             onChange={formik.handleChange}
+            error={formik.touched.nombre && Boolean(formik.errors.nombre)}
+            helperText={formik.touched.nombre && formik.errors.nombre}
           />
-          {formik.touched.accept && formik.errors.accept && <span className='error'>{formik.errors.accept}</span>}
-        </div>
-        <input type='submit' value='Enviar' className='btn-submit' />
+
+          {/* Email */}
+          <TextField
+            label='Email'
+            id='email'
+            name='email'
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+
+          {/* Aceptar términos */}
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <FormControlLabel
+              control={
+                <Checkbox id='accept' name='accept' checked={formik.values.accept} onChange={formik.handleChange} />
+              }
+              label='Aceptar términos'
+            />
+            {formik.touched.accept && formik.errors.accept && (
+              <FormHelperText error>{formik.errors.accept}</FormHelperText>
+            )}
+          </Box>
+
+          {/* Submit */}
+          <Button type='submit' variant='contained' color='primary'>
+            Enviar
+          </Button>
+        </Box>
       </form>
     </div>
   );
